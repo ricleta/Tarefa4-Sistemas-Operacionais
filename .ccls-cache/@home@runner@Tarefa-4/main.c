66b92b080908c746801644d0bcba6a1164b8d781
ricleta <47068518+ricleta@users.noreply.github.com>
2022-11-02 17:04:36 +0000

    files organized

--- .ccls-cache/@home@runner@Tarefa-4/main.c
+++ .ccls-cache/@home@runner@Tarefa-4/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void) 
-{
-  printf("Hello World\n");
-  return 0;
-}
+// int main(void) 
+// {
+//   printf("Hello World\n");
+//   return 0;
+// }
